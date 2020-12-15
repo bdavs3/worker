819f0dd3bd4601fc651e8682ee4580528ff51193
Ben Davis <bdavs3000@gmail.com>
2020-12-15 11:43:12 -0700

    Implement basic API structure.
    • Use net/http to serve the endpoints that will be called by each CLI commands.
    • Add skeleton handler functions to the worker library for each endpoint.

--- server/api.go
+++ server/api.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/bdavs3/worker/server/worker"
+)
+
+const port = "8080"
+
+func main() {
+	http.HandleFunc("/jobs/run", worker.Run)
+	http.HandleFunc("/jobs/status", worker.Status)
+	http.HandleFunc("/jobs/out", worker.Out)
+	http.HandleFunc("/jobs/kill", worker.Kill)
+
+	http.ListenAndServe(":"+port, nil)
 }
--- server/worker/worker.go
+++ server/worker/worker.go
@@ -1,1 +1,27 @@
 package worker
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Job struct {
+	command string
+	args    []string
+}
+
+func Run(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Run")
+}
+
+func Status(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Status")
+}
+
+func Out(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Out")
+}
+
+func Kill(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Kill")
+}

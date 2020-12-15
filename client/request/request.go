99df7273d279215d97395f92dcf1006f63fec748
Ben Davis <bdavs3000@gmail.com>
2020-12-15 12:40:27 -0700

    Configure request library to make 'Run' API call
    • Fill function body of 'Run' and add its documentation comment.
    • Declare 'host' and 'port' constants for the request library.

--- client/request/request.go
+++ client/request/request.go
@@ -1,13 +1,57 @@
 package request
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
+	"github.com/bdavs3/worker/server/worker"
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	host = "http://localhost"
+	port = "8080"
+)
+
+// Run passes a job to the worker library for execution.
 func Run(c *cli.Context) error {
-	fmt.Println("Run")
+	if c.NArg() == 0 {
+		return errors.New("No job supplied to 'run' command")
+	}
+
+	job := worker.Job{
+		Command: c.Args().Get(0),
+		Args:    c.Args().Slice()[1:],
+	}
+
+	requestBody, err := json.Marshal(job)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := http.Post(
+		host+":"+port+"/jobs/run",
+		"application/json",
+		bytes.NewBuffer(requestBody),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(body))
+
 	return nil
 }
 

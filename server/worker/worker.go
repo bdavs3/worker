ce4eb3f230c2d6ea951d407422f8491f531ed7ba
Ben Davis <bdavs3000@gmail.com>
2020-12-15 15:13:12 -0700

    Add documentation comments to the worker library

--- server/worker/worker.go
+++ server/worker/worker.go
@@ -5,23 +5,28 @@ import (
 	"net/http"
 )
 
+// Job represents a Linux process that the client passes to the server.
 type Job struct {
 	Command string
 	Args    []string
 }
 
+// Run initiates the execution of a process passed by the client.
 func Run(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Run")
 }
 
+// Status queries the log for the status of a given process.
 func Status(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Status")
 }
 
+// Out queries the log for the output of a given process.
 func Out(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Out")
 }
 
+// Kill terminates a given process.
 func Kill(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Kill")
 }

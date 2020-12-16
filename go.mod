d00c6fc426f5c43a4f98edf1861a0bf41f1192ee
Ben Davis <bdavs3000@gmail.com>
2020-12-15 18:22:03 -0700

    Add authentication functionality to auth.go.
    • Add AuthenticateUser(...) function, which validates user credentials by invoking bcrypt to check whether a request's pw matches the hashed version of the correct pw when passing it through a one-way hashing function.

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/bdavs3/worker
 go 1.15
 
 require github.com/gorilla/mux v1.8.0
+
+require golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9
--- server/auth/auth.go
+++ server/auth/auth.go
@@ -1,1 +1,44 @@
 package auth
+
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// AuthenticateUser verifies the credentials in the Authorization header of a request by utilizing the bcrypt hashing function.
+func AuthenticateUser(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if username, pw, ok := r.BasicAuth(); !ok || !validate(username, pw) {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Account Invalid"`)
+			w.WriteHeader(401)
+		} else {
+			handler(w, r)
+		}
+	}
+}
+
+func validate(username, pw string) bool {
+	if username == "default_user" {
+		// TODO: Store user credentials in a secure database.
+		// User passwords should be hashed before storing them in the DB.
+		hash, err := hashPassword("123456")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		return checkPasswordHash(pw, hash)
+	}
+	return false
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}

846bf3c18a3d25ccb1fb28a696fbe2204131f91c
Ben Davis <bdavs3000@gmail.com>
2020-12-18 22:39:11 -0700

    Enforce rate-limiting per user in the auth layer

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -2,14 +2,14 @@ package auth
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/time/rate"
 )
 
-// TODO (next): Enhance the rate-limiting authorization check by making it
-// user-specific. Add two additional authorization checks: (1) Only allow
+// TODO (next): Add two additional authorization checks: (1) Only allow
 // users to access endpoints for jobs they created and (2) Enforce local-only
 // request origin policy.
 
@@ -18,18 +18,26 @@ const (
 	globalHash     = "$2a$10$P7GoVlD0fEu14OWE76dGzude2NLw0pi05Gzar6rm1b.oD04lcvyaq"
 )
 
-var limiter = rate.NewLimiter(5, 1) // Allows a request every 200ms.
+// TODO (out of scope): The userLimiter map should be cleaned up
+// periodically to free memory. This could be done by keeping
+// track of users' "last seen" times and having a background goroutine
+// delete the oldest entries.
+var userLimiters = make(map[string]*rate.Limiter)
+var mu sync.Mutex
 
 // Secure enforces user authentication and rate limiting before allowing a
 // request to reach a given endpoint.
 func Secure(router *mux.Router) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if username, pw, ok := r.BasicAuth(); !ok || !validate(username, pw) {
+		username, pw, ok := r.BasicAuth()
+
+		if !ok || !validate(username, pw) {
 			w.WriteHeader(http.StatusUnauthorized)
 			w.Write([]byte("Invalid credentials. Access denied."))
 			return
 		}
-		if !limiter.Allow() { // TODO (next): Enforce per-user.
+
+		if !getUserLimiter(username).Allow() {
 			w.WriteHeader(http.StatusTooManyRequests)
 			w.Write([]byte("Too many requests."))
 			return
@@ -47,4 +55,17 @@ func validate(username, pw string) bool {
 		return err == nil
 	}
 	return false
+}
+
+func getUserLimiter(username string) *rate.Limiter {
+	mu.Lock()
+	defer mu.Unlock()
+
+	limiter, exists := userLimiters[username]
+	if !exists {
+		limiter = rate.NewLimiter(5, 1) // Allows a request every 200ms.
+		userLimiters[username] = limiter
+	}
+
+	return limiter
 }

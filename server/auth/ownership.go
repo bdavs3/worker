1391938b59736ca5c50ac2828018b9156a00dcb4
Ben Davis <bdavs3000@gmail.com>
2020-12-28 16:34:04 -0700

    Authorize users only when attempting to access a resource that they own

--- server/auth/ownership.go
+++ server/auth/ownership.go
@@ -13,10 +13,21 @@ func newOwnershipTracker() *ownershipTracker {
 	}
 }
 
-func (ot *ownershipTracker) setOwner() {
+func (ot *ownershipTracker) setOwner(username, id string) {
+	ot.mu.Lock()
+	defer ot.mu.Unlock()
 
+	if _, ok := ot.ownerships[username]; !ok {
+		ot.ownerships[username] = make(map[string]struct{})
+	}
+	ot.ownerships[username][id] = struct{}{}
 }
 
-func (ot *ownershipTracker) isOwner() bool {
+func (ot *ownershipTracker) isOwner(username, id string) bool {
+	ot.mu.Lock()
+	defer ot.mu.Unlock()
 
+	_, ok := ot.ownerships[username][id]
+
+	return ok
 }

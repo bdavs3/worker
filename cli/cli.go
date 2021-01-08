124e5f118237cabca06504ab1a37149970b0d65d
Ben Davis <bdavs3000@gmail.com>
2021-01-08 10:46:07 -0700

    Move the cli worker service to the same file as the cli.

--- cli/cli.go
+++ cli/cli.go
@@ -1,16 +1,19 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/bdavs3/worker/services"
+	"github.com/bdavs3/worker/client"
+	"github.com/bdavs3/worker/worker"
 
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	workerService, err := services.NewWorkerService()
+	workerService, err := newWorkerService()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,25 +28,25 @@ func main() {
 				Name:    "run",
 				Aliases: []string{"r"},
 				Usage:   "give the server a Linux process to execute",
-				Action:  workerService.Run,
+				Action:  workerService.run,
 			},
 			{
 				Name:    "status",
 				Aliases: []string{"s"},
 				Usage:   "get the status of a services by providing its id",
-				Action:  workerService.Status,
+				Action:  workerService.status,
 			},
 			{
 				Name:    "out",
 				Aliases: []string{"o"},
 				Usage:   "get the output of a services by providing its id",
-				Action:  workerService.Out,
+				Action:  workerService.out,
 			},
 			{
 				Name:    "kill",
 				Aliases: []string{"k"},
 				Usage:   "terminate a services by providing its id",
-				Action:  workerService.Kill,
+				Action:  workerService.kill,
 			},
 		},
 	}
@@ -52,4 +55,92 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type workerService struct {
+	Client *client.Client
+}
+
+func newWorkerService() (*workerService, error) {
+	client, err := client.NewClient()
+	if err != nil {
+		return nil, err
+	}
+
+	workerService := &workerService{
+		Client: client,
+	}
+
+	return workerService, nil
+}
+
+func (ws *workerService) run(ctx *cli.Context) error {
+	if ctx.NArg() == 0 {
+		return errors.New("no job supplied to 'run' command")
+	}
+
+	job := worker.Job{
+		Command: ctx.Args().Get(0),
+		Args:    ctx.Args().Slice()[1:],
+	}
+
+	responseBody, err := ws.Client.PostJob(job)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(responseBody)
+
+	return nil
+}
+
+func (ws *workerService) status(ctx *cli.Context) error {
+	if ctx.NArg() != 1 {
+		return errors.New("no job id supplied to 'status' command")
+	}
+
+	id := ctx.Args().Get(0)
+
+	responseBody, err := ws.Client.GetJobStatus(id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(responseBody)
+
+	return nil
+}
+
+func (ws *workerService) out(ctx *cli.Context) error {
+	if ctx.NArg() != 1 {
+		return errors.New("no job ID supplied to 'out' command")
+	}
+
+	id := ctx.Args().Get(0)
+
+	responseBody, err := ws.Client.GetJobOutput(id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(responseBody)
+
+	return nil
+}
+
+func (ws *workerService) kill(ctx *cli.Context) error {
+	if ctx.NArg() != 1 {
+		return errors.New("no job ID supplied to 'kill' command")
+	}
+
+	id := ctx.Args().Get(0)
+
+	responseBody, err := ws.Client.KillJob(id)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(responseBody)
+
+	return nil
 }
